c2fd34a6593c59e860a10d0e88fe69fe5e83be73
zivilesideikiene <martynovaitezivile@gmail.com>
2023-04-25 19:49:01 +0300

    pakeisti modeliu atvaizdavimo pavadinimai(vns ir dgs)

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -5,19 +5,38 @@ import uuid
 class Genre(models.Model):
     name = models.CharField(verbose_name="Pavadinimas", max_length=50)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Zanras"
+        verbose_name_plural = "Zanrai"
 
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length=50)
     last_name = models.CharField(verbose_name="Pavarde", max_length=50)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Autorius"
+        verbose_name_plural = "Autoriai"
+
 class Book(models.Model):
-    title = models.CharField(verbose_name="Pavadainimas", max_length=100)
+    title = models.CharField(verbose_name="Pavadinimas", max_length=100)
     summary = models.TextField(verbose_name="Aprasymas", max_length=2000)
     isbn = models.CharField(verbose_name="ISBN", max_length=13)
     author = models.ForeignKey(to="Author", verbose_name="Autorius", on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField(to="Genre", verbose_name="Zanras")
+
+    def __str__(self):
+        return f"{self.title} ({self.author})"
+
+    class Meta:
+        verbose_name = "Knyga"
+        verbose_name_plural = "Knygos"
+
 
 class BookInstance(models.Model):
     book = models.ForeignKey(to="Book", verbose_name="Knyga", on_delete=models.CASCADE)
@@ -32,5 +51,9 @@ class BookInstance(models.Model):
     )
     status = models.CharField(verbose_name="Busena", max_length=1, choices=LOAN_STATUS, blank=True, default="a")
 
+    def __str__(self):
+        return f"{self.book.title}: {self.uuid} ({self.due_back}) - {self.status}"
 
-
+    class Meta:
+        verbose_name = "Knygos egzempliorius"
+        verbose_name_plural = "Knygos egzemplioriai"
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,7 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'lt'
+
 
 TIME_ZONE = 'Europe/Vilnius'
 
